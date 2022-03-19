84564df6a5a73590d9f3d65bcb80c83ae0d52765
Logan O'Connell <logan.w.oconnell@gmail.com>
2022-03-18 21:17:44 -0400

    Update TestGhostValidMoves.java

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -11,9 +11,9 @@ public class TestGhostValidMoves extends TestCase {
 
 		Ghost ghost = frame2.addGhost(new Location(3, 3), "red", Color.red);
 
-		//assertTrue(ghost.get_valid_moves().size() == 3);
+		assertFalse(ghost.get_valid_moves().size() == 3);
 		assertTrue(ghost.get_valid_moves().contains(new Location(2, 3)));
-		assertTrue(ghost.get_valid_moves().contains(new Location(4, 3)));
+		assertFalse(ghost.get_valid_moves().contains(new Location(4, 3)));
 		assertTrue(ghost.get_valid_moves().contains(new Location(3, 4)));
 	}
 }
